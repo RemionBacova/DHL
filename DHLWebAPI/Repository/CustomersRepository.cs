40e7a4e198ab0fe389ad45b33e3b98e03a72ff0d
Jonel Laho <jonel.laho@fshnstudent.info>
2020-05-05 23:59:13 +0100

    Repository+IRepository(TblCustomers)

---
+++ DHLWebAPI/Repository/CustomersRepository.cs
@@ -0,0 +1,55 @@
+﻿using DHLWebAPI.Data;
+using DHLWebAPI.Models;
+using DHLWebAPI.Repository.IRepository;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DHLWebAPI.Repository
+{
+    public class CustomersRepository : ICustomersRepository
+    {
+        private readonly DHLContext db;
+
+        //Dependenct Injection for DHLContext
+        public CustomersRepository(DHLContext db)
+        {
+            this.db = db;
+        }
+        //Below are different crud operations implemented by the ICustomersRepository interface
+        public bool CreateCustomers(TblCustomers customers)
+        {
+            db.TblCustomers.Add(customers);
+            return Save();
+        }
+
+        public bool DeleteCustomers(TblCustomers customers)
+        {
+            db.TblCustomers.Remove(customers);
+            return Save();
+        }
+
+        public TblCustomers GetCustomers(int customerID)
+        {
+            return db.TblCustomers.FirstOrDefault(o => o.IdCustomer.Equals(customerID));
+        }
+
+        public ICollection<TblCustomers> GetCustomers()
+        {
+            return db.TblCustomers.ToList();
+        }
+
+        public bool UpdateCustomers(TblCustomers customers)
+        {
+            db.TblCustomers.Update(customers);
+            return Save();
+        }
+
+        public bool Save()
+        {
+            return db.SaveChanges() >= 0 ? true : false;
+        }
+    }
+}
---
+++ DHLWebAPI/Repository/IRepository/ICustomersRepository.cs
@@ -0,0 +1,18 @@
+﻿using DHLWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DHLWebAPI.Repository.IRepository
+{
+    interface ICustomersRepository
+    {
+        ICollection<TblCustomers> GetCustomers();
+        TblCustomers GetCustomers(int customerID);
+        bool CreateCustomers(TblCustomers customers);
+        bool UpdateCustomers(TblCustomers customers);
+        bool DeleteCustomers(TblCustomers customers);
+        bool Save();
+    }
+}
