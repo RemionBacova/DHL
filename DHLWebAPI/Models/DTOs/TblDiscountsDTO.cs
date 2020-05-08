1492e979c3e33b47748762bb70c83627399fa96f
Jonel Laho <jonel.laho@fshnstudent.info>
2020-05-08 17:30:39 +0100

    .

--- DHLWebAPI/Models/DTOs/TblDiscountsDTO.cs
+++ DHLWebAPI/Models/DTOs/TblDiscountsDTO.cs
@@ -10,7 +10,7 @@ namespace DHLWebAPI.Models.DTOs
         public int IdDiscount { get; set; }
         public string DiscountTitle { get; set; }
         public string DiscountDescription { get; set; }
-        public byte DiscountType { get; set; }
+        public int DiscountType { get; set; }
         public float DiscountPerc { get; set; }
         public DateTime DiscountStartDate { get; set; }
         public DateTime DiscountEndDate { get; set; }
